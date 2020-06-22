772f4ec1fdc7569d9eb5730b9377592c5fef2f0c
Christopher Magennis <uucmagennis@gmail.com>
2020-06-22 12:44:49 -0700

    Add files via upload

---
+++ User_input.py
@@ -0,0 +1,13 @@
+# Basic user input
+# We can provide a prompt within the parenthesis
+# Here we're directly assigning the input to a variable, user_name
+
+user_name = input("Enter your name: ")
+
+print("Hello " + user_name + "!")
+
+# We can also prompt the user to enter multiple pieces of information
+
+user_age = input("Enter your age: ")
+
+print("So your name is " + user_name + " and you are " + user_age + " years old.")
